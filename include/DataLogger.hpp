6e99dbfb1fddf4ce88e7ad7e8262ba2b82143fe0
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2024-09-16 13:47:28 +0200

    Work i has started

--- include/DataLogger.hpp
+++ include/DataLogger.hpp
@@ -1,99 +1,52 @@
-#ifndef __SENSOR_HPP__
-#define __SENSOR_HPP__
+#ifndef __DATALOGGER_HPP__
+#define __DATALOGGER_HPP__
 
-#include <cstdint>
-#include <string>
-#include <concepts>
-#include <print>
-#include <random>   // for the "fakes" sensors 
+#include <exception>
+#include <chrono>
 
-//----------------------------------------------------------------------------
-template <typename OutputType>
-concept Numeric = std::is_arithmetic_v<OutputType>; /**< Use of concepts to ensure Output value is a numeric type */
+using namespace     std::chrono_literals;
 
-template <Numeric OutputType>
-class Sensor{
+using date_of_event_t = std::chrono::time_point<std::chrono::system_clock>;
 
-private:
-    const uint32_t UID{};   /**< Each Sensor has a Unique ID (32 bits) */
-    const std::string   Unit{}; /**< Unit of the output  */
-    OutputType outputValue{};
-protected:
+enum class Level_t {INFO, WARNING, ERROR};
+
+struct  event_t{
+    date_of_event_t     date{};
+    std::string         what{};
+    Level_t             Level{};
+
+};
+
+
+class   DataLogger{
+    private:
+        std::chrono::system_clock   internalClock{}; 
+    protected:
+        [[nodiscard]]   date_of_event_t getInternalClock() noexcept {return this->internalClock.now();};
+    public:
+        DataLogger() = default;
+        virtual ~DataLogger() = default;
+
+        virtual void    AddEvent(event_t &evt , bool setInternalTime = false) = 0;
+        virtual void    dumpLog() = 0;
+};
+
+class   VectorDataLogger : public DataLogger{
+    private:
+        std::vector<event_t>    log{};
     
-    void setOutputValue(OutputType newValue) noexcept {this->outputValue = newValue;};
-    
-public:
-    Sensor() = delete;  /**< Ctor by default is deleted */
-    Sensor(uint32_t uid, const std::string &unit):UID{uid},Unit{unit}{};
-    virtual ~Sensor() = default;    /**< Dtor is defaulted */
-
-    [[nodiscard]] const std::string getUnit() const noexcept {return this->Unit;};
-    [[nodiscard]] uint32_t  getUID() const noexcept {return this->UID;};
-    [[nodiscard]] OutputType getOutputValue() const noexcept {return this->outputValue;};
-
-    virtual void    elaborateNewValue() = 0;   /**< Pure virtual method -> class is now abract */ 
-
-    void    dump() const noexcept {std::println("{} {}",this->getOutputValue,this->getUnit());};
-};
-//----------------------------------------------------------------------------
-class ADT7310 : public Sensor<uint16_t>{
     public:
-        enum class ADT7310Mode {MODE13BITS, MODE16BITS};
-    private:
-        ADT7310Mode  Mode{ADT7310Mode::MODE13BITS};
-    public:
-        ADT7310() = delete;
-        explicit ADT7310(uint32_t uid):Sensor<uint16_t>(uid,"LSB 0.0625°C (Signed 13 bits mode) or LSB 0.0078°C (Signed 16 bits mode)"){};  
-        virtual ~ADT7310() = default;
+        VectorDataLogger() = default;
+        virtual ~VectorDataLogger() = default;
 
-        void        setMode(ADT7310Mode Mode) noexcept {this->Mode = Mode;};
-        ADT7310Mode getMode() const noexcept {return this->Mode;}; 
+        virtual void    AddEvent(event_t &evt, bool setInternalTime = false) override;     
 
-        virtual void    elaborateNewValue() override;
-
-        double  getConvertedTemperature() const noexcept;
 };
-//----------------------------------------------------------------------------
-template <Numeric OutputType>
-class GenericTempCelciusSensor : public Sensor<OutputType>{
-    private:
-
-    public:
-        GenericTempCelciusSensor() = delete;
-        virtual ~GenericTempCelciusSensor() = default;
-
-        explicit GenericTempCelciusSensor(uint32_t uid):Sensor<OutputType>(uid,"°C"){};
-
-        virtual void    elaborateNewValue() = 0;
-};
-//----------------------------------------------------------------------------
-constexpr   uint32_t    TC74_UUID{0x7400484F};
-
-class TC74 : public GenericTempCelciusSensor<int8_t>{
-    private:
-        const uint8_t i2c_address{};
-    public:
-        TC74() = delete;
-        virtual ~TC74() = default;
-        explicit TC74(uint8_t addr):GenericTempCelciusSensor<int8_t>{TC74_UUID},i2c_address{addr}{};
-
-        uint8_t getI2Caddress() const noexcept {return this->i2c_address;};
-
-        virtual void    elaborateNewValue() override;
-};
-//----------------------------------------------------------------------------
-constexpr   uint32_t    LM35_UUID{0x35000000};
-
-class LM35 : public GenericTempCelciusSensor<double>{
-    private:
-        
-    public:
-        LM35():GenericTempCelciusSensor<double>{LM35_UUID}{};
-        virtual ~LM35() = default;
-        
-        virtual void    elaborateNewValue() override;
-};
-//----------------------------------------------------------------------------
 
 
-#endif  /*  __SENSOR_HPP__  */
+
+
+
+
+
+#endif  /*  __DATALOGGER_HPP__  */
--- src/testDataLogger.cpp
+++ src/testDataLogger.cpp
@@ -1,55 +1,32 @@
 #include <print>
 
-#include "Sensor.hpp"
+#include "DataLogger.hpp"
 
-constexpr   uint32_t    SIMPLE_SENSOR_UID{0xAA000001};
 
 int main(void){
-    std::println("--- Test Sensor Classes ---");
 
-    TC74    tc74a0(0x48);
+    event_t myEvent{};
 
-    std::println("Sensor UID = {0:08x}",tc74a0.getUID());
-    std::println("Sensor Unit : {0}",tc74a0.getUnit());
+    myEvent.Level = Level_t::WARNING;
+    myEvent.what = "Warning event";
+        
+    VectorDataLogger    myVectorDataLogger{};
 
-    for (uint8_t i=0;i<10;++i){
-        tc74a0.elaborateNewValue();
-        std::print(" {0:d} ",tc74a0.getOutputValue());
+    try{
+        myVectorDataLogger.AddEvent(myEvent);
     }
-    
-    std::println("");
-
-    LM35    lm35ca{};
-    std::println("Sensor UID = {0:08x}",lm35ca.getUID());
-    std::println("Sensor Unit : {0}",lm35ca.getUnit());
-
-    for (uint8_t i=0;i<10;++i){
-        lm35ca.elaborateNewValue();
-        std::print(" {0:.02f} ",lm35ca.getOutputValue());
+    catch(const std::invalid_argument &e){
+        std::println("Exception catched ({}) OK.",e.what());
     }
 
-    std::println("");
+    myEvent.date = std::chrono::system_clock{}.now();
 
-    ADT7310 myAdt(0x11223344);
-    std::println("Sensor UID = {0:08x}",myAdt.getUID());
-    std::println("Sensor Unit : {0}",myAdt.getUnit());
-
-    for (uint8_t i=0;i<10;++i){
-        myAdt.elaborateNewValue();
-        std::println(" {0:04x} = {1:.02f} ",myAdt.getOutputValue(),myAdt.getConvertedTemperature());
+    try{
+        myVectorDataLogger.AddEvent(myEvent);
     }
-
-    std::println("");
-
-    myAdt.setMode(ADT7310::ADT7310Mode::MODE16BITS);
-
-    for (uint8_t i=0;i<10;++i){
-        myAdt.elaborateNewValue();
-        std::println(" {0:04x} = {1:.02f}",myAdt.getOutputValue(),myAdt.getConvertedTemperature());
+    catch(const std::invalid_argument &e){
+        std::println("Exception catched ({}) NOT OK.",e.what());
     }
-
-    std::println("");
-
 
 }
 
